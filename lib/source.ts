abb59700c3fb39d2b856e69b42725e678036b673
Rupak Kumar Goura <rupakgoura18@gmail.com>
2026-07-18 14:15:34 +0530

    IP

--- lib/source.ts
+++ lib/source.ts
@@ -1,8 +1,8 @@
-import { blog as blogCollection } from 'collections/server';
+import { blog as blogCollection, meta } from 'collections/server';
 import { loader } from 'fumadocs-core/source';
 import { toFumadocsSource } from 'fumadocs-mdx/runtime/server';
 
 export const blog = loader({
   baseUrl: '/blog',
-  source: toFumadocsSource(blogCollection, []),
+  source: toFumadocsSource(blogCollection, meta),
 });
--- source.config.ts
+++ source.config.ts
@@ -1,14 +1,13 @@
-import { defineCollections, defineConfig, frontmatterSchema } from 'fumadocs-mdx/config';
+import { defineDocs, frontmatterSchema } from 'fumadocs-mdx/config';
 import { z } from 'zod';
 
-export const blog = defineCollections({
-  type: 'doc',
+export const { docs: blog, meta } = defineDocs({
   dir: 'content/blog',
-  schema: frontmatterSchema.extend({
-    date: z.string().date().or(z.date()), // Validate standard YYYY-MM-DD string or Date object
-    description: z.string().max(200),
-    tags: z.array(z.string()).optional(),
-  }),
+  docs: {
+    schema: frontmatterSchema.extend({
+      date: z.string().date().or(z.date()), // Validate standard YYYY-MM-DD string or Date object
+      description: z.string().max(200),
+      tags: z.array(z.string()).optional(),
+    }),
+  }
 });
-
-export default defineConfig();
